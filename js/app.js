af802fb6e2f0b93cc8c36280811eb08bdf0b523a
keny <keny@gis.tw>
2014-05-16 21:29:24 +0800

    do some fix

--- js/app.js
+++ js/app.js
@@ -17,6 +17,13 @@ app.controller('AnchorCTRL',
       $scope.mAnchorList = [];
       $scope.mAnchor = [];
       $scope.showTable = false;
+      $scope.panelData=[
+          {sn:"PL",name:"預鑄格梁-PL"},{sn:"SL",name:"場鑄格梁-SL"},
+          {sn:"CR",name:"十字型格梁-CR"},{sn:"CB",name:"連梁-CB"},
+          {sn:"PR",name:"排樁-PR"},{sn:"RC",name:"面版-RC"},
+          {sn:"RW",name:"擋土牆-RW"}
+
+      ];
 
       $scope.$on('ngGridEventEndCellEdit', function (data) {
           //console.log(data);
@@ -43,7 +50,7 @@ app.controller('AnchorCTRL',
               //{field: 'free', displayName: '自由段', enableCellEdit: true},
               {field: 'header', displayName: '錨碇段', cellTemplate:'<div class="ngCellText colt{{$index}}"><input type="number" style="text-align: center;width: 100px"  data-ng-model="COL_FIELD"  min="0"/></div>'},
               {field: 'weight', displayName: '設計荷重', cellTemplate:'<div class="ngCellText colt{{$index}}"><input type="number" style="text-align: center;width: 100px"  data-ng-model="COL_FIELD"  min="0"/></div>'},
-              {field: '',displayName: '刪除', cellTemplate: '<input type="button" value="刪除" ng-click="removeRow()" />' }
+              {field: '',displayName: '刪除', cellTemplate: '<div class="ngCellText"><input type="button" value="刪除" ng-click="removeRow()" /></div>' }
 
               ]
       };
@@ -74,10 +81,11 @@ app.controller('AnchorCTRL',
 
           var tempAnchor =
           {
-              sn:$scope.area.toUpperCase()+$scope.level+$scope.row,
-              area:$scope.area.toUpperCase(),
+              sn:$scope.panelType.sn+$scope.area+$scope.level+$scope.row,
+              ptype:$scope.panelType.sn,
+              area:$scope.area,
               level:$scope.level,
-              row:$scope.row.toUpperCase(),
+              row:$scope.row,
               num:$scope.num
           }
 
@@ -85,7 +93,8 @@ app.controller('AnchorCTRL',
               if($scope.mAnchor[i].sn==tempAnchor.sn){
                   $scope.mAnchor.splice(i,1);
                   $scope.mAnchorList =$scope.mAnchorList.filter(function(entry){
-                      if(entry.area==tempAnchor.area&&
+                      if( entry.ptype==tempAnchor.ptype&&
+                          entry.area==tempAnchor.area&&
                           entry.level==tempAnchor.level&&
                           entry.row==tempAnchor.row
                           ){
@@ -100,35 +109,38 @@ app.controller('AnchorCTRL',
           }
 
           $scope.mAnchor.push({
-              sn:$scope.area.toUpperCase()+$scope.level+$scope.row.toUpperCase(),
-              area:$scope.area.toUpperCase(),
+              sn:$scope.panelType.sn+$scope.area+$scope.level+$scope.row,
+              ptype:$scope.panelType.sn,
+              area:$scope.area,
               level:$scope.level,
-              row:$scope.row.toUpperCase(),
+              row:$scope.row,
               num:$scope.num
           });
           for(var i=0;i<$scope.num;i++){
               if(i<9){
                   $scope.mAnchorList.push({
+                      ptype:$scope.panelType.sn,
                       free:$scope.free,
                       header:$scope.header,
                       weight:$scope.weight,
-                      area:$scope.area.toUpperCase(),
+                      area:$scope.area,
                       level:$scope.level,
-                      row:$scope.row.toUpperCase(),
+                      row:$scope.row,
                       num:i+1,
-                      sn:""+$scope.area.toUpperCase()+$scope.level+$scope.row.toUpperCase()+"0"+(i+1)
+                      sn:$scope.panelType.sn+"-"+$scope.area+$scope.level+$scope.row+"0"+(i+1)
                   });
 
               }else{
                   $scope.mAnchorList.push({
+                      ptype:$scope.panelType.sn,
                       free:$scope.free,
                       header:$scope.header,
                       weight:$scope.weight,
-                      area:$scope.area.toUpperCase(),
+                      area:$scope.area,
                       level:$scope.level,
-                      row:$scope.row.toUpperCase(),
+                      row:$scope.row,
                       num:i+1,
-                      sn:""+$scope.area.toUpperCase()+$scope.level+$scope.row.toUpperCase()+(i+1)
+                      sn:$scope.panelType.sn+"-"+$scope.area+$scope.level+$scope.row+(i+1)
                   });
               }
 
@@ -138,9 +150,10 @@ app.controller('AnchorCTRL',
           //$("#content").innerHTML = $scope.mAnchorList;
 
           //document.getElementById("mycontent").innerHTML = JSON.stringify($scope.mAnchorList);
-          console($scope.mAnchorList.length);
+          console.log($scope.mAnchorList.length);
 
           var gridHeight = $scope.mAnchorList.length>0?$scope.mAnchorList.length * 35:50;
+          if(gridHeight<200)gridHeight=200;
           $scope.mystyle={height:gridHeight+'px'};
 
       }
@@ -153,19 +166,31 @@ app.controller('AnchorCTRL',
           $scope.mAnchorList.splice(index, 1);
           //document.getElementById("mycontent").innerHTML = JSON.stringify($scope.mAnchorList);
           var gridHeight = $scope.mAnchorList.length>0?$scope.mAnchorList.length * 35:50;
+          if(gridHeight<200)gridHeight=200;
           $scope.mystyle={height:gridHeight+'px'};
       };
 
 
-      $scope.numberValid = function($value,index){
-          console.log(free+" "+result);
-          var result = true;
-          if ($value) {
-            //  result = $scope.mAnchorList[index]["free"] >= $value;
-          }
+//      $scope.numberValid = function($value,index){
+//          console.log(free+" "+result);
+//          var result = true;
+//          if ($value) {
+//            //  result = $scope.mAnchorList[index]["free"] >= $value;
+//          }
+//
+//          return result;
+//      };
 
-          return result;
-      };
+
+      $scope.$watch('area', function() {
+        if($scope.area)
+          $scope.area = $scope.area.toUpperCase().replace(/\s+/g,'');
+      });
+
+      $scope.$watch('row', function() {
+         if($scope.area)
+          $scope.row = $scope.row.toUpperCase().replace(/\s+/g,'');
+      });
 
 
 
